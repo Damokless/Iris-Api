364dc3fb4db195d2015dc685a61fd0bf7c1d886d
Damokless <damokless@protonmail.com>
2022-10-14 13:38:56 +0200

    adding comment in register service

--- prisma/services/register.ts
+++ prisma/services/register.ts
@@ -1,7 +1,11 @@
+// Imports libs and imports api services
 import { PrismaClient } from '@prisma/client';
 import argon2 from 'argon2';
 
+/* Creating a new instance of the PrismaClient. */
 const prisma = new PrismaClient();
+
+/* Setting the hashing config for the password. */
 const hashingConfig = { // based on OWASP cheat sheet recommendations (as of March, 2022)
   parallelism: 1,
   memoryCost: 2 ** 16,
@@ -10,6 +14,12 @@ const hashingConfig = { // based on OWASP cheat sheet recommendations (as of Mar
   type: argon2.argon2id,
 };
 
+/**
+ * Creating a new user in the DB with the email, username, and password provided in the request
+ * body
+ * @param {any} body - any - This is the body of the request.
+ * @returns The user object
+ */
 export default async function register(body: any) {
   const user = await prisma.users.create({
     data: {
