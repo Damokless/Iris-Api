af5eb9e380057bea43a082410c12404f9bc6f26f
Damokless <damokless@protonmail.com>
2022-10-14 13:32:00 +0200

    adding comments in index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+// Imports libs and imports api services
 import Fastify from 'fastify';
 import fs from 'fs';
 import register from './prisma/services/register';
 import login from './prisma/services/login';
 
+/* Creating a new instance of the Fastify server with HTTPS protocol. */
 const fastify = Fastify({
   logger: true,
   https: {
@@ -11,10 +13,12 @@ const fastify = Fastify({
   },
 });
 
+/* Creating a new routes for the server. */
 fastify.post('/register', async (req) => register(req.body));
 fastify.post('/login', async (req) => login(req.body));
 
-fastify.listen({ port: 8080 }, (err, address) => {
+/* Listening to the port 4000 and if there is an error it will log it and exit the process. */
+fastify.listen({ port: 4000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
