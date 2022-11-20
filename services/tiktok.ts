317925ee326258757613c9c267840a42c0bd6212
Damokless <damokless@protonmail.com>
2022-11-20 03:45:15 +0100

    testing tiktok api

---
+++ services/tiktok.ts
@@ -0,0 +1,13 @@
+export default function tiktok(): string {
+  const csrfState = Math.random().toString(36).substring(2);
+
+  let url = 'https://www.tiktok.com/auth/authorize/';
+
+  url += '?client_key=awagptjbwxwwq0rm';
+  url += '&scope=user.info.basic,video.list';
+  url += '&response_type=code';
+  url += '&redirect_uri=https://localhost:4000/tiktokAUTHCallback';
+  url += `&state=${csrfState}`;
+
+  return url;
+}
---
+++ services/tiktokCallback.ts
@@ -0,0 +1,3 @@
+export default function tiktokCallback(body: any) {
+  console.log(body);
+}
